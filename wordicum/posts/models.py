1cf148552c33edc9ba52aa2b509e489857f58123
v-kapi <115559104+v-kapi@users.noreply.github.com>
2023-10-23 22:56:05 +0300

    Update models.py
    translate

--- wordicum/posts/models.py
+++ wordicum/posts/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Post(models.Model):
     text = models.TextField()
-    pub_date = models.DateTimeField("Дата публикации", auto_now_add=True)
+    pub_date = models.DateTimeField("Publication date", auto_now_add=True)
 
     def __str__(self):
         return self.text
