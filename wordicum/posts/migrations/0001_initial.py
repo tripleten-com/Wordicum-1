bbfe61d59c3d8dc43c219c321467e0cb972dd9c3
v-kapi <115559104+v-kapi@users.noreply.github.com>
2023-10-23 22:56:31 +0300

    Update 0001_initial.py
    translate

--- wordicum/posts/migrations/0001_initial.py
+++ wordicum/posts/migrations/0001_initial.py
@@ -16,7 +16,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('text', models.TextField()),
-                ('pub_date', models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации')),
+                ('pub_date', models.DateTimeField(auto_now_add=True, verbose_name='Publication date')),
             ],
         ),
     ]
